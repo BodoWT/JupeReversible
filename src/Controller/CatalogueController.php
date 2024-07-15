51b2c99052d5863d97d58727fdb2aa4b6133b49b
BodoWT <victorien.lague@gmail.com>
2024-07-15 21:20:57 +0200

    test

--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -15,4 +15,40 @@ class CatalogueController extends AbstractController
             'controller_name' => 'CatalogueController',
         ]);
     }
+
+    #[Route('/catalogue/homme', name: 'app_homme')]
+    public function homme(): Response
+    {
+        return $this->render('homme/index.html.twig');
+    }
+
+    #[Route('/catalogue/femme', name: 'app_femme')]
+    public function femme(): Response
+    {
+        return $this->render('femme/index.html.twig');
+    }
+
+    #[Route('/catalogue/enfant', name: 'app_enfant')]
+    public function enfant(): Response
+    {
+        return $this->render('enfant/index.html.twig');
+    }
+
+    #[Route('/catalogue/grandeTaille', name: 'app_grandeTaille')]
+    public function grandeTaille(): Response
+    {
+        return $this->render('drande_taille/index.html.twig');
+    }
+
+    #[Route('/catalogue/bijoux', name: 'app_bijoux')]
+    public function bijoux(): Response
+    {
+        return $this->render('bijoux/index.html.twig');
+    }
+
+    #[Route('/catalogue/accessoires', name: 'app_accessoires')]
+    public function accessoires(): Response
+    {
+        return $this->render('accessoires/index.html.twig');
+    }
 }
